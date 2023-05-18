b082fec758a69bf702f4604ecef86671efdafffa
KishanKumarPatel <kishan.janovis@gmail.com>
2023-05-18 13:14:39 +0530

    css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,4 @@
-// /** @type {import('tailwindcss').Config} */
-// // module.exports = {
-// //   content: [],
-// //   theme: {
-// //     extend: {},
-// //   },
-// //   plugins: [],
-// // }
-
-// module.exports = {
-//   purge: [],
-//   darkMode: false, // or 'media' or 'class'
-//   theme: {
-//     extend: {},
-//   },
-//   variants: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
